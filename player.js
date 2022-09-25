acfcb502a43cc005f9ffe7c0fcb8f99b1db690c1
Anna Peterson <reveragb@gmail.com>
2022-09-25 09:43:55 -0600

    Add player class

--- player.js
+++ player.js
@@ -1,7 +1,17 @@
 class Player {
-    constructor() {
-
-    }
-}
-//player1 = human
-//player2 = computer --> random index
+    constructor(name, icon) {
+        this.name = name;
+        this.icon = icon;
+        this.choice = "";
+        this.wins = 0
+    };
+    takeTurn(choiceArray) {
+        if (this.name === "Human") { 
+            var playerChoice = playerChoice;
+        } else if (this.name === "Computer") { 
+            var randomIndex = Math.floor(Math.random() * choiceArray.length);
+            var randomChoice = choiceArray[randomIndex];
+            this.choice = randomChoice;
+        };
+    };
+};
