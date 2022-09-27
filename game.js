2129b212e3e72256a0be89ed258ca9128c3d240a
Anna Peterson <reveragb@gmail.com>
2022-09-26 19:57:45 -0600

    Fix errors and add changes to html and css

--- game.js
+++ game.js
@@ -1,37 +1,12 @@
-var gameType = "";
-var rock = "rock";
-var paper = "paper";
-var scissors = "scissors";
-var lizard = "lizard";
-var spock = "spock";
-var classicChoices = [rock, paper, scissors];
-var difficultChoices = [rock, paper, scissors, lizard, spock];
-var choiceArray = "";
 
 class Game {
-    constructor(gameType) {
-        this.gameType = gameType;
+    constructor(gameChoices) {
+        this.gameChoices = gameChoices;
         this.player1 = new Player("Human", "👤");
         this.player2 = new Player("Computer", "🖥");
         this.player1Choice = this.player1.choice;
         this.player2Choice = this.player2.choice;
         this.fighters = [this.player1Choice, this.player2Choice];
         this.winner = "";
-    };
-    findGameType() {
-        if (this.gameType === "classic") {
-            chooseClassicWinner();
-        }else if (this.gameType === "difficult") {
-            chooseDifficultWinner();
-        };
-    };
-};
-
-function chooseGameType(type) {
-    choiceArray = "";
-    if (gameType === type) {
-        choiceArray = classicChoices;
-    } else {
-        choiceArray = difficultChoices;
     };
 };
