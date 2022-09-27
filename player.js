b30efdf0781f8186059b73cd04aeb821167b5233
Anna Peterson <reveragb@gmail.com>
2022-09-27 11:46:40 -0600

    Delete pseudocode

--- player.js
+++ player.js
@@ -7,7 +7,7 @@ class Player {
     };
     takeTurn(choiceArray) {
         if (this.name === "Human") { 
-            var playerChoice = playerChoice; //this.choice = playerChoice;
+            var playerChoice = playerChoice;
         } else if (this.name === "Computer") { 
             var randomIndex = Math.floor(Math.random() * choiceArray.length);
             var randomChoice = choiceArray[randomIndex];
